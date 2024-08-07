f3d905488bf2c9b9427a462f3604498026cd0bb9
zakaria.narjis <zakaria.narjis.97@gmail.com>
2024-08-07 03:05:09 +0200

    add td3 and sac

--- src/train_.py
+++ src/train_.py
@@ -25,7 +25,7 @@ from modularl.policies.gaussian_policy import GaussianPolicy
 from modularl.q_functions import SAQNetwork
 from modularl.replay_buffers import ReplayBuffer
 import torch.optim as optim
-
+from create_agent import CreateAgent
 
 class Config(object):
     def __init__(self, dictionary):
@@ -179,7 +179,7 @@ def main():
             )
         ),
     )
-
+    create_agent = CreateAgent()
     random.seed(sac_config.seed)
     np.random.seed(sac_config.seed)
     torch.manual_seed(sac_config.seed)
@@ -274,52 +274,10 @@ def main():
                 train_opt["device"],
             )
         )
-        actor = GaussianPolicy(
-            observation_shape=train_opt["obs_shape"],
-            action_shape=train_opt["action_shape"],
-            high_action=train_opt["high_action"],
-            low_action=train_opt["low_action"],
-        )
-        qf1 = SAQNetwork(
-            observation_shape=train_opt["obs_shape"],
-            action_shape=train_opt["action_shape"],
-        )
-        qf2 = SAQNetwork(
-            observation_shape=train_opt["obs_shape"],
-            action_shape=train_opt["action_shape"],
-        )
-        replay_buffer = ReplayBuffer(
-            buffer_size=train_opt["buffer_size"],
-        )
-        q_optimizer = optim.Adam(
-            list(qf1.parameters()) + list(qf2.parameters()),
-            lr=train_opt["q_lr"],
-            weight_decay=train_opt["q_weight_decay"],
-        )
-        actor_optimizer = optim.Adam(
-            list(actor.parameters()),
-            lr=train_opt["policy_lr"],
-            weight_decay=train_opt["policy_weight_decay"],
-        )
-        agent = SAC(
-            actor=actor,
-            qf1=qf1,
-            qf2=qf2,
-            actor_optimizer=actor_optimizer,
-            q_optimizer=q_optimizer,
-            replay_buffer=replay_buffer,
-            gamma=train_opt["gamma"],
-            entropy_lr=train_opt["q_lr"],
-            tau=train_opt["tau"],
-            batch_size=train_opt["batch_size"],
-            learning_starts=train_opt["learning_starts"],
-            device=train_opt["device"],
-            target_network_frequency=train_opt["target_network_frequency"],
-            policy_frequency=train_opt["policy_frequency"],
-            target_entropy=-train_opt["action_shape"],
-            writer=writer,
-        )
-        agent.init()
+        if train_opt["algo"] == "sac":
+            agent = create_agent.sac(train_opt, writer)
+        elif train_opt["algo"]=="td3":
+            agent = create_agent.td3(train_opt, writer)
         agent_list.append(agent)
 
     with torch.cuda.device(options["device"]):
